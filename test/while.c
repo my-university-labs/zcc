9f043c2518386dba14e39727063062998bee3348
张东昌 <dongchangzhang@gmail.com>
2017-05-10 12:01:55 +0800

    save-

--- test/while.c
+++ test/while.c
@@ -1,13 +1,14 @@
 int main() {
   int b = 1;
   int a = 10;
-  while (a > b) {
+  while (a > b && b < a) {
     a = a + b;
     while (a < b) {
       b = a + b;
     }
+    if (a > b)
+      a = 2;
   }
-  if (a > b)
-    a = 2;
+
   return 0;
 }
