da62af74e5728adcd031aee6b4be5fa62c42c823
dongchangzhang <dongchangzhang@gmail.com>
2017-05-15 14:58:19 +0800

    暂时不再更新

--- src/main.cpp
+++ src/main.cpp
@@ -18,10 +18,13 @@ int main(int argc, char* argv[])
         print_error(-1, -1, NONE_INPUT);
         return 1;
     }
+
+    std::cout << "--------------- 3 addr code --------------------" << std::endl;
     Parser parser(argv[1], argv[2], argv[3]);
     parser.run();
     auto codes = parser.get_codes();
     auto stmg = parser.get_stmg();
+    std::cout << "-------------------- Asm ------------------------" << std::endl;
 
     AsmCreater asm_creater(codes, stmg);
 
